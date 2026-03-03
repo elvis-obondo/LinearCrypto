1a7414afd59d400e653e4a4e05446c5d1d371e79
Tail-Gate <tailgatesportapp@gmail.com>
2026-03-03 10:17:07 -0500

    Synthetic asset foundation

--- data_manipulation/covariance_matrix.py
+++ data_manipulation/covariance_matrix.py
@@ -5,13 +5,13 @@ correlation_list = []
 
 def main():
     sol_data,kmno_data=get_market_data()
-    max_lag = 4
-    for i in range(max_lag+1):
-        combined_matrix = np.vstack([sol_data[i:],kmno_data[:None if i==0 else -i]])
-        correlation_matrix = np.corrcoef(combined_matrix)
-        correlation_list.append(correlation_matrix)
     
-    print(f"Successfully done correlation matrix")
+    
+    combined_matrix = np.vstack([sol_data,kmno_data])
+    correlation_matrix = np.corrcoef(combined_matrix)
+    correlation_list.append(correlation_matrix)
+    
+   
     return correlation_list
     
 
--- data_manipulation/eigen-decomposition.py
+++ data_manipulation/eigen-decomposition.py
@@ -1,15 +1,14 @@
 import numpy as np
 from covariance_matrix import main
 import pprint
-correlation_list= main()
-i=0
-for cmatrix in correlation_list:
 
-    eigenvalues,eigenvectors = np.linalg.eig(cmatrix)
-    print(f"correlation matrix {i}:")
-    pprint.pprint(cmatrix)
-    print("Eigenvalues")
-    pprint.pprint(eigenvalues)
-    print("Eigenvectors")
-    pprint.pprint(eigenvectors)
-    i+=1
+correlation_list= main()
+
+def get_eigendata(correlation_list):
+    eigenvalues,eigenvectors = np.linalg.eig(correlation_list)
+    return eigenvalues,eigenvectors
+
+if __name__ == "__main__":
+    get_eigendata(correlation_list)
+
+
